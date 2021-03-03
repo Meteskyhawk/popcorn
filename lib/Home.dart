8d54e718e12b566ce86e88a1a688d95081df6c7b
BetaISyn5243 <49427559+BetaISyn5243@users.noreply.github.com>
2021-03-03 18:40:53 +0300

    .

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,7 +8,7 @@ import 'package:popcorn/loadData.dart';
 
 
 Future<LoadData> apiCall() async{
-  final response = await http.get("http://www.omdbapi.com/?t=as");
+  final response = await http.get("http://www.omdbapi.com/?apikey=9f2b5808&t=as");
   if (response==200){
     return LoadData.fromJson(json.decode(response.body));
   }else{
