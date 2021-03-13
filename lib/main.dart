f3c219bde5c9af9235e224817517ed0222136188
sahte9okuz <mthngkdgn@Gmail.com>
2021-03-13 23:19:17 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,108 @@
 import 'package:flutter/material.dart';
-import 'package:popcorn/Login.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:flutter_signin_button/button_list.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:popcorn/Home.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as JSON;
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Popcorn',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Login(),
-    );
+  State<StatefulWidget> createState() {
+    return _MyAppState();
   }
 }
 
+class _MyAppState extends State<MyApp> {
+  bool _isLoggedIn = true;
+  Map userProfile;
+  final facebookLogin = FacebookLogin();
+
+  _loginWithFB() async {
+    final result = await facebookLogin.logInWithReadPermissions(['email']);
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final token = result.accessToken.token;
+        final graphResponse = await http.get(
+            'https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=${token}');
+        final profile = JSON.jsonDecode(graphResponse.body);
+        print(profile);
+        setState(() {
+          userProfile = profile;
+          _isLoggedIn = true;
+        });
+        break;
+
+      case FacebookLoginStatus.cancelledByUser:
+        setState(() => _isLoggedIn = false);
+        break;
+      case FacebookLoginStatus.error:
+        setState(() => _isLoggedIn = false);
+        break;
+    }
+  }
+
+  _logout() {
+    facebookLogin.logOut();
+    setState(() {
+      _isLoggedIn = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: new HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                alignment: Alignment(-.4, 0),
+                image: NetworkImage(
+                    'https://image.freepik.com/free-photo/rusty-metallic-wall-background-yellow-old-paint_23-2148742531.jpg'),
+                fit: BoxFit.cover),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Popcorn",
+                  style: TextStyle(
+                      fontSize: 55.0,
+                      color: Colors.white,
+                      height: 5,
+                      fontStyle: FontStyle.italic),
+                ),
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://image.freepik.com/free-vector/realistic-popcorn-bucket_98292-1416.jpg"),
+                  radius: 60.0,
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                SignInButton(
+                  Buttons.Facebook,
+                  onPressed: () =>
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(builder: (context) => Home()),
+                      ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
